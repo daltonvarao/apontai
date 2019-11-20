9e40b3f4f3894ab3260352942ff6c3a4156df450
daltonvarao <daltonphellipe@gmail.com>
2019-11-20 19:35:07 -0300

    add all to deploy

---
+++ app/models/reclamacao.py
@@ -0,0 +1,11 @@
+from .db import db
+
+
+class Reclamacao(db.Model):
+    __tablename__ = 'reclamacao'
+
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(100))
+    local = db.Column(db.String(100))
+    tempo = db.Column(db.String(50))
+    desc = db.Column(db.String(500))
